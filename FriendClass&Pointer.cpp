c764ffd9667fc6b3126d64ef65e3e7f293d9904b
ririndeismillia <ririndeismillia@gmail.com>
2026-04-27 21:18:07 +0700

    classPublicBelahKetupat

--- FriendClass&Pointer.cpp
+++ FriendClass&Pointer.cpp
@@ -8,4 +8,19 @@ private:
     double d1, d2; 
     double s1, s2; 
 
+public:
+    void input() {
+        cout << "=== Layang-Layang ===" << endl;
+        cout << "Masukkan diagonal 1: ";
+        cin >> d1;
+        cout << "Masukkan diagonal 2: ";
+        cin >> d2;
+        cout << "Masukkan sisi 1: ";
+        cin >> s1;
+        cout << "Masukkan sisi 2: ";
+        cin >> s2;
+    }
+
 };
+
+    
